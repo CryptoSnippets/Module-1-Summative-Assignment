adba6d3482ebe0f5ffa199b154cc6b624baf7bab
Alexis Hoffman <azguard@iinet.net.au>
2016-07-14 07:37:02 +0800

    Complete CRUD model methods

--- app/models/racer.rb
+++ app/models/racer.rb
@@ -1,6 +1,17 @@
 class Racer
   include Mongoid::Document
-  
+  attr_accessor :id, :number, :first_name, :last_name, :gender, :group, :secs
+
+	def initialize(params={})
+		@id=params[:_id].nil? ? params[:id] : params[:_id].to_s
+		@number=params[:number].to_i
+		@first_name=params[:first_name]
+		@last_name=params[:last_name]
+		@gender=params[:gender]
+		@group=params[:group]
+		@secs=params[:secs].to_i
+	end
+
   # convenience method for access to client in console
   def self.mongo_client
   	Mongoid::Clients.default
@@ -19,5 +30,42 @@ class Racer
   	all = all.limit(limit) if !limit.nil?
   	return all
   end
+
+	def self.find id
+		result=collection.find(:_id => BSON::ObjectId.from_string(id)).first
+		return result.nil? ? nil : Racer.new(result)
+	end
+
+	def save
+		result=self.class.collection
+						.insert_one(
+							number:@number, 
+							first_name:@first_name, 
+							last_name:@last_name, 
+							gender:@gender, 
+							group:@group, 
+							secs:@secs)
+		@id=result.inserted_id.to_s
+	end
+
+	def update(params)
+		@number=params[:number].to_i
+		@first_name=params[:first_name]
+		@last_name=params[:last_name]
+		@gender=params[:gender]
+		@group=params[:group]
+		@secs=params[:secs].to_i
+
+		params.slice!(:number, :first_name, :last_name, :gender, :group, :secs)
+		self.class.collection
+						.find(:_id => BSON::ObjectId.from_string(@id))
+						.update_one(params)
+	end
+
+	def destroy
+		self.class.collection
+							.find(number:@number)
+							.delete_one
+	end
 end
 
